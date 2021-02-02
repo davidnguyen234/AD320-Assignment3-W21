636764cf06fd13fd534242d1ea9d4a7750781070
Caleb Mcolin <Caleb.McOlin@seattlecolleges.edu>
2021-02-01 17:40:54 -0800

    first commit

--- routes/cities.js
+++ routes/cities.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const myData = require('../data/cities.json');
+
+
+router.get('/:id', function (req, res, next) {
+    let myData = require('../data/cities.json');
+    let reqID = req.params.id;
+    for (let i = 0; i < myData.length; i++) {
+        if (myData[i].id == reqID) { 
+            res.send(myData[i]);
+        }
+    }
+});
 
 module.exports = router;
