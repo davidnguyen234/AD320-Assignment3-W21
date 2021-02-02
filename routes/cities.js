c3ad4951e801b1472b1f1e3f82781a5f6e40cf67
Caleb Mcolin <Caleb.McOlin@seattlecolleges.edu>
2021-02-02 12:06:25 -0800

    changed route.js

--- routes/cities.js
+++ routes/cities.js
@@ -7,7 +7,7 @@ router.get('/:id', function (req, res, next) {
     let myData = require('../data/cities.json');
     let reqID = req.params.id;
     for (let i = 0; i < myData.length; i++) {
-        if (myData[i].id == reqID) { 
+        if (myData[i].id == reqID) {
             res.send(myData[i]);
         }
     }
